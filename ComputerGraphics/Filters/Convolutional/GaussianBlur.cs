05dcd889c2e12b8b6696a0f616c3c2b5df2ab243
Kiryl Volkau <volkaukiryl@gmail.com>
2021-03-09 04:08:43 +0100

    Update kernel for GB.

--- ComputerGraphics/Filters/Convolutional/GaussianBlur.cs
+++ ComputerGraphics/Filters/Convolutional/GaussianBlur.cs
@@ -6,9 +6,9 @@ namespace ComputerGraphics.Filters.Convolutional
         {
             kernel = new double[,]
             {
-                {1, 2, 1,},
-                {2, 4, 2,},
-                {1, 2, 1,},
+                {0, 1, 0,},
+                {1, 4, 1,},
+                {0, 1, 0,},
             };
 
             return kernel;
